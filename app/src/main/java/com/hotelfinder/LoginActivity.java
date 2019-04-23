fdc0f25732cd157c05e94b8392ce93b207181204
Manojkumardangi <dangimanoj372@gmail.com>
2019-04-23 17:38:16 +0545

    splash_screen added

--- app/src/main/java/com/hotelfinder/LoginActivity.java
+++ app/src/main/java/com/hotelfinder/LoginActivity.java
@@ -1,14 +1,29 @@
 package com.hotelfinder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView create_account;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getSupportActionBar().setTitle("Login Form");
+        create_account = findViewById(R.id.tv_signup);
+        create_account.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.tv_signup) {
+            final Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+            startActivity(intent);
+
+        }
     }
 }
--- app/src/main/java/com/hotelfinder/SignupActivity.java
+++ app/src/main/java/com/hotelfinder/SignupActivity.java
@@ -1,14 +1,31 @@
 package com.hotelfinder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class SignupActivity extends AppCompatActivity {
+public class SignupActivity extends AppCompatActivity implements View.OnClickListener {
+TextView already_member;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.signup_activity);
+        setContentView(R.layout.activity_signup);
         getSupportActionBar().setTitle("SignUp Form");
+        already_member=findViewById(R.id.tv_login);
+        already_member.setOnClickListener(this);
+
     }
-}
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId()==R.id.tv_login){
+            final  Intent intent=new Intent(SignupActivity.this,LoginActivity.class);
+            startActivity(intent);
+        }
+
+        }
+    }
+
